157688d73aaae23cc82c598184911511fdfc1cb2
sdxiphos <serhatdemir1492@gmail.com>
2020-11-10 19:37:03 +0300

    8.DayEnd

--- ceaser_ciphol.py
+++ ceaser_ciphol.py
@@ -1,2 +1,30 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#test
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text,shift):
+    encrypt_text=''
+    for item in text:
+        if item in alphabet:
+            encrypt_text += alphabet[alphabet.index(item)+shift]
+    return encrypt_text
+
+def decrypt(text,shift):
+    decrypt_text=''
+    for item in text:
+        if item in alphabet:
+            decrypt_text += alphabet[alphabet.index(item)-shift]
+    return decrypt_text
+
+
+if direction=='encode':
+
+    print(f'Your encrypted letter: {encrypt(text,shift)}')
+elif direction=='decode':
+    shift=2
+    print(f'Your decrypted letter: {decrypt(text,shift)}')
+else:
+    print('You entered wrong type! Try again! \n')
+    pass
