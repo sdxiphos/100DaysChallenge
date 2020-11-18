cbf401e516aa8581b09c81cff6283ebf9872abf3
sdxiphos <serhatdemir1492@gmail.com>
2020-11-19 00:04:30 +0300

    '16.Day'

--- coffee_machine/coffe_machine_controller.py
+++ coffee_machine/coffe_machine_controller.py
@@ -1,1 +1,29 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+import time
+
+
+coffee_on = True
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+while coffee_on:
+    customer_choice = input('What do you want? \n1.)latte\n2.)espresso\n3.)cappuccino\n')
+
+
+
+
+    if customer_choice != 'report':
+        drink = menu.find_drink(customer_choice)
+        if coffee_maker.is_resource_sufficient(drink) :
+            if money_machine.make_payment(drink.cost) :
+                coffee_maker.make_coffee(drink)
+
+    elif customer_choice == 'quit':
+        coffee_on = False
+    else:
+        coffee_maker.report()
+        money_machine.report()
 
