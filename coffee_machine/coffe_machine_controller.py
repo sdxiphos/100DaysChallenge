9ea679ad02e60abfe73dfc85439485839d190b4d
sdxiphos <serhatdemir1492@gmail.com>
2020-11-19 00:06:32 +0300

    '16.Day'

--- coffee_machine/coffe_machine_controller.py
+++ coffee_machine/coffe_machine_controller.py
@@ -15,15 +15,16 @@ while coffee_on:
 
 
 
-    if customer_choice != 'report':
-        drink = menu.find_drink(customer_choice)
-        if coffee_maker.is_resource_sufficient(drink) :
-            if money_machine.make_payment(drink.cost) :
-                coffee_maker.make_coffee(drink)
+    if customer_choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+
 
     elif customer_choice == 'quit':
         coffee_on = False
     else:
-        coffee_maker.report()
-        money_machine.report()
+        drink = menu.find_drink(customer_choice)
+        if coffee_maker.is_resource_sufficient(drink) :
+            if money_machine.make_payment(drink.cost) :
+                coffee_maker.make_coffee(drink)
 
